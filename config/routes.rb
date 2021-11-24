2721b21fdab841a26b5d5ff57715291eb7eb9187
Haji Mammadov <hmammado@gmail.com>
2021-11-24 18:23:11 +0100

    Created a show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'cities#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cities, only: %i[new create] do
+  resources :cities, only: %i[new create show] do
     resources :itineraries, only: %i[new create]
   end
 
