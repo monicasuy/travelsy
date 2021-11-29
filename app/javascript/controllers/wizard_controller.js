b6fad210430a4b05471f60dae28ddcf482e9f315
monica suy <monicasuy96@gmail.com>
2021-11-29 15:47:01 +0100

    added previous button to the form

--- app/javascript/controllers/wizard_controller.js
+++ app/javascript/controllers/wizard_controller.js
@@ -5,12 +5,20 @@ export default class extends Controller {
 
   nextStep(event) {
     const nextStep = event.target.dataset.nextStep;
-    const previousStep = event.target.dataset.previousStep;
     const actualStep = nextStep - 1;
 
     // remove "is--step-0" from container
     this.containerTarget.classList.remove(`is--step-${actualStep}`)
     // add "is--step-1" from container
     this.containerTarget.classList.add(`is--step-${nextStep}`)
+  }
+
+  previousStep(event) {
+    const previousStep = Number(event.target.dataset.previousStep);
+    const actualStep = previousStep + 1;
+    // remove "is--step-1" from container
+    this.containerTarget.classList.remove(`is--step-${actualStep}`)
+    // add "is--step-0" to container
+    this.containerTarget.classList.add(`is--step-${previousStep}`)
   }
 }
