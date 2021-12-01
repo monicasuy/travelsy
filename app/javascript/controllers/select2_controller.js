4efc6806715b1b6de38f6ffd10253a918ee60d3d
monica suy <monicasuy96@gmail.com>
2021-12-01 18:20:13 +0100

    added dropdown and styled it

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 import $ from "jquery";
 import "select2";
-// import 'select2/dist/css/select2.min.css'; <- Uncomment this line if you are using Rails
+import 'select2/dist/css/select2.min.css';
 
 export default class extends Controller {
   connect() {
-    $(this.element).select2();
+    $(this.element).select2({
+      width: "100%",
+    });
   }
 }
